32a46a619bdb9fdf661b40eae30ee1966fc4921a
mykhailo-tyshynskyi <tihan.cs@gmail.com>
2026-05-24 21:15:03 +0300

    done

---
+++ src/js/render-functions.js
@@ -0,0 +1,65 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+export let lightbox = new SimpleLightbox('.gallery-item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const murkup = images
+    .map(img => {
+      const {
+        tags,
+        comments,
+        downloads,
+        views,
+        likes,
+        webformatURL: preview,
+        largeImageURL,
+      } = img;
+      return `<li class="gallery-item">
+	<a class="gallery-link" href="${largeImageURL}">
+		<img 
+		  class="gallery-image" 
+		  src="${preview}"
+		  alt="${tags}" 
+		/>
+         <ul class="img-data">
+    <li class="img-data-item">
+    <span class="accent">Likes</span>${likes}
+    </li>
+      <li class="img-data-item">
+    <span class="accent">Views</span>${views}
+    </li>
+      <li class="img-data-item">
+    <span class="accent">Comments</span>${comments}
+    </li>
+      <li class="img-data-item">
+    <span class="accent">Downloads</span>${downloads}
+    </li>
+    </ul>
+	</a>
+   
+</li>
+`;
+    })
+    .join('');
+
+  return murkup;
+}
+
+export function clearGallery() {
+  const container = document.querySelector('.container');
+  container.innerHTML = '';
+}
+
+export function showLoader() {
+  const loader = document.querySelector('.loader');
+  loader.classList.remove('isHiden');
+}
+
+export function hideLoader() {
+  const loader = document.querySelector('.loader');
+  loader.classList.add('isHiden');
+}
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import { createGallery } from './js/render-functions';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { lightbox } from './js/render-functions';
+import iziToast from 'izitoast';
+import { clearGallery } from './js/render-functions';
+import { showLoader } from './js/render-functions';
+import { hideLoader } from './js/render-functions';
+
+const form = document.querySelector('.form');
+const input = document.querySelector('[name="search-text"]');
+const container = document.querySelector('.container');
+
+form.addEventListener('submit', handleSearchSubmit);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  clearGallery();
+  showLoader();
+  const query = input.value.trim();
+  if (query === '') {
+    return;
+  } else {
+    getImagesByQuery(query)
+      .then(data => {
+        if (data.length === 0) {
+          throw new Error();
+        }
+        const murkup = createGallery(data);
+        hideLoader();
+        container.innerHTML = murkup;
+        lightbox.refresh();
+      })
+      .catch(err => {
+        hideLoader();
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'center',
+        });
+      });
+  }
+  event.target.reset();
+}
